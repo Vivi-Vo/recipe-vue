f1f14e164f999387fb164f7fe7483a0b318c5e50
Vivi-Vo <vivivo5015@gmail.com>
2020-08-02 18:50:31 -0400

    Render instruction steps

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -3,7 +3,7 @@
     <div v-for="dish in dishes" :key="dish.id">
       <v-img
         :src="dish.image"
-        @click.stop="dialog=true;getRecipeIngredients(dish.id);selectedDish=dish.title"
+        @click.stop="dialog=true;getRecipeIngredients(dish.id);selectedDish=dish"
       />
       <p>{{dish.title}}</p>
     </div>
@@ -11,12 +11,22 @@
     <v-dialog width="600px" v-model="dialog">
       <v-card>
         <v-card-title>
-          <span class="headline">{{selectedDish}}</span>
+          <span class="headline">{{selectedDish.title}}</span>
         </v-card-title>
         <v-card-text>
           <ul>
             <li v-for="item in ingredients" :key="item.index">{{item}}</li>
           </ul>
+        </v-card-text>
+        <v-btn
+          raised
+          color="purple lighten-4"
+          @click="getInstruction(selectedDish.id)"
+        >Get Instructions</v-btn>
+        <v-card-text v-if="instructions.length">
+          <ol>
+            <li v-for="steps in instructions" :key="steps.number">{{steps.step}}</li>
+          </ol>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -39,21 +49,29 @@ export default {
       instructions: [],
       api_key: "8056c060793247a1a67cd935a4434d5f",
       url_base: "https://api.spoonacular.com/recipes",
-      selectedDish: "",
+      selectedDish: {},
       ingredients: []
     };
   },
   methods: {
     getRecipeIngredients: function(id) {
-      console.log("calling");
+      this.ingredients.length = 0;
+      this.instructions.length = 0;
       const url = `${this.url_base}/${id}/ingredientWidget.json?apiKey=${this.api_key}`;
       fetch(url)
         .then(res => res.json())
         .then(result => {
-          this.ingredients.length = 0;
           result.ingredients.forEach(element => {
             this.ingredients.push(element.name);
           });
+        });
+    },
+    getInstruction: function(id) {
+      const url = `${this.url_base}/${id}/analyzedInstructions?&apiKey=${this.api_key}&stepBreakdown=true`;
+      fetch(url)
+        .then(res => res.json())
+        .then(result => {
+          this.instructions = result[0].steps;
         });
     }
   }
